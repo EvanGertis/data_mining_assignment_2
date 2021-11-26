5eedf5caf4d0b0f07d3d78bcb707afe49b3d56fa
Evan Gertis <physevangertis@gmail.com>
2021-11-26 15:03:54 -0500

    part b added

--- assignment2.py
+++ assignment2.py
@@ -1,8 +1,7 @@
 from numpy.core.defchararray import count
 import pandas as pd
 import numpy as np
-import numpy as np
-from math import ceil, floor, log2
+import math
 from sklearn.decomposition import PCA
 from numpy import linalg as LA
 from sklearn.tree import DecisionTreeClassifier
@@ -21,173 +20,71 @@ def calculate_metrics(tp, tn, fn, p, n, fp):
 def display_metrics(accuracy, error_rate, sensitivity, precision, specificity):
     print(f'Accuracy: {accuracy}, Error_rate:{error_rate}, Sensitivity:{sensitivity}, Precision:{precision}, specificity:{specificity}')
 
-def mc(columnName,training_set):
-    column = training_set[columnName]
+# Step 1- Calculate MC (Message Conveyed) for the given dataset (let us call it file TF) in reference to  the class attribute 
+# MC(TF) = -p1*log2(p1) - p2*log2(p2) 
+def mc(classAttribute,attribute,training_set):
+    column = training_set[classAttribute]
+
+    if attribute:
+        column = training_set[training_set[classAttribute] == attribute] 
+
     probs = column.value_counts(normalize=True)
     messageConveyed = -1*np.sum(np.log2(probs)*probs)
-    # print(f'mc {messageConveyed}')
     return messageConveyed
 
-def isUnique(s):
-    a = s.to_numpy() # s.values (pandas<0.24)
-    return (a[0] == a).all()
+def wmc(classAttribute,attribute,training_set):
+    attributeCount = len(training_set[training_set[classAttribute] == attribute].index)
+    total          = len(training_set[classAttribute].index)
+    print(f'{attributeCount}/{total}')
+    return attributeCount/total
 
-def ID3(root,training_set,test_set):
+def ID3(root,training_set,test_set, threshold, g):
 
-<<<<<<< HEAD
+    highestGainAttribute = ""
+    highestGainValue     = -math.inf
+    for classAttribute, values in training_set.iteritems():
+        messageConveyed = mc(classAttribute, attribute=None, training_set=training_set)
+        print(f"{classAttribute} mc: {messageConveyed}")
 
-    if(root == ""):
-        # Step 1- Calculate MC (Message Conveyed) for the given data set in reference to the class attribute
-        print(f'Step 1- Calculate MC (Message Conveyed) for the given data set in reference to the class attribute')
-        # MC = -p1*log2(p1) - p2*log2(p2)
-        # For n classes MC = -p1log2(p1) - p2*log2(p2)-...-pn*log2(pn)
+        attributes = training_set[classAttribute].unique()
+        print(f"{classAttribute}\n")
+        weightedMessageConveyed = 0
+        for attribute in attributes:
+            weight = wmc(classAttribute, attribute, training_set)
+            messageConveyed = mc(classAttribute, attribute, training_set)
+            print(f"wmc({attribute}) = {weight}")
+            weightedMessageConveyed += weight*messageConveyed
 
-        # For each column calculate the gain.
-        numberOfColumns = 0
-        mcDictionary = {}
-        print('***********************************')
-        print('For each column calculate the gain.')
-        for (columnName, columnData) in training_set.iteritems():
-            messageConveyed = mc(columnName,training_set)
-            mcDictionary.update({columnName:round(messageConveyed)})
-            numberOfColumns+=1
-        print('***********************************')
-        print(f'numberOfColumns {numberOfColumns}')
-        print(f'mcDictionary {mcDictionary}')
-
-
-
-        # The column with the highest gain is the root.
-        print(f'The column with the highest gain is the root.')
-        values = mcDictionary.values()
-        max_value = max(values)
-        print(f'The max value is {max_value}')
-        # print(f'The max value, {max_value}, is associated with column {columnWithMaximumInformationGain}')
-        val_list = list(values)
-        columnWithMaximumInformationGain = list(mcDictionary.keys())[list(mcDictionary.values()).index(max_value)]
-        print(f'The max value, {max_value}, is associated with column {columnWithMaximumInformationGain}')
-
-        # select the max value from the gain array
-        # this is the new root
-        root =  columnWithMaximumInformationGain
-        print(f'root is {root}')
-        print("******************************************")
-        print("**************   ROOT   ******************")
-        print(f"TF is {root}**********************")
-        print("******************************************")
-        print(f'isUnique = {isUnique(training_set[root])}')
-        if(isUnique(training_set[root])):
-            return   
-=======
-    mcDictionary = {}
-    # Step 1- Calculate MC (Message Conveyed) for the given data set in reference to the class attribute
-    print(f'Step 1- Calculate MC (Message Conveyed) for the given data set in reference to the class attribute')
-    # MC = -p1*log2(p1) - p2*log2(p2)
-    # For n classes MC = -p1log2(p1) - p2*log2(p2)-...-pn*log2(pn)
-
-    # For each column calculate the gain.
-    numberOfColumns = 0
-    print('***********************************')
-    print('For each column calculate the gain.')
-    for (columnName, columnData) in training_set.iteritems():
-        messageConveyed = mc(columnName,training_set)
-        mcDictionary.update({columnName:round(messageConveyed)})
-        numberOfColumns+=1
-    print('***********************************')
-    print(f'numberOfColumns {numberOfColumns}')
-    print(f'mcDictionary {mcDictionary}')
-
-
-    # The column with the highest gain is the root.
-    print(f'The column with the highest gain is the root.')
-    values = mcDictionary.values()
-    max_value = max(values)
-    print(f'The max value is {max_value}')
-    # print(f'The max value, {max_value}, is associated with column {columnWithMaximumInformationGain}')
-    val_list = list(values)
-    columnWithMaximumInformationGain = list(mcDictionary.keys())[list(mcDictionary.values()).index(max_value)]
-    print(f'The max value, {max_value}, is associated with column {columnWithMaximumInformationGain}')
-
-    # select the max value from the gain array
-    # this is the new root
-    root =  columnWithMaximumInformationGain
-    print(f'root is {root}')
-    print("******************************************")
-    print("**************   ROOT   ******************")
-    print(f"TF is {root}**********************")
-    print("******************************************")
-    print(f'isUnique = {isUnique(training_set[root])}')
-    if(isUnique(training_set[root])):
-        return   
->>>>>>> fa8afdb6f1bea639e09e81ed89d314dd13a58298
+        print(f'wmc({classAttribute}) = {weightedMessageConveyed}')
+        gain = messageConveyed - weightedMessageConveyed
+        print(f'MC - wmc({classAttribute}) = {messageConveyed} - {weightedMessageConveyed} = {gain}')
+        if gain > highestGainValue:
+            highestGainAttribute = classAttribute
+            highestGainValue     = gain
     
-    # Step 2 - Repeat for every attribute
-    print(f'Step 2 - Repeat for every attribute')
-    # Loop 1
-    attribute = ""
-    maximum       = 0 
-    for (F, columnData) in training_set.iteritems():
-        print(f'processing attribute {F}')
-        # Loop 2
-        total = 0
-        uniques = training_set[F].unique()
-        for k in uniques:
-            print(f'processing branch {k} for {F}')
-            # Calculate MC for column
-            messageConveyed = mc(F,training_set)
-
-            # Calculate the weight for F
-            F_D    = training_set[F].count()
-            TF_D   = training_set[root].count()
-
-            weight = F_D/TF_D
-            total = weight*messageConveyed
-        gain = abs(mc(root,training_set) - total)
-        if(gain > maximum):
-            attribute = F
-            maximum   = gain 
-        print(f"gain: {gain} for {F}")
-    
-    print(f'attribute {attribute} has the max gain of {gain}')
-    print(f'removing {attribute}')
-    root = attribute
-    print(f'new root {root} has branches {training_set[root].unique()}')
-    print(f'root is {root}')
-    print("******************************************")
-    print("**************   ROOT   ******************")
-    print(f"TF is {root}**********************")
-    print("******************************************")
-    unique_values = training_set[root].unique()
-    datasets = []
-    for unique_value in unique_values:
-        print(f'processing for file : {unique_value} ')
-        df_1 = training_set[training_set[attribute] > unique_value]
-        datasets.append(df_1)
-
-    # del training_set[attribute]
-    
-    # Step 3 - Examine dataset of each leaf
-    print(f'Step 3 - Examine dataset of each leaf')
-    print(f'number of datasets {len(datasets)}')
-    print("*****************")
-    print("printing datasets")
-    print("*****************")
-    dataframes = {}
-    for df in datasets:
-        print(f'Step 4 - for {attribute} dataset check is marked "split"')
-        if(df[attribute].is_unique):
-            print(f'all values are the same no split')
+    print(f'winner is {highestGainAttribute} with gain of {highestGainValue}')
+    root = highestGainAttribute
+    leaves = training_set[root].unique()
+    splits = {}
+    for leaf in  leaves:
+        print(f'leaf: {leaf} of root: {root}')
+        if training_set[training_set[root] == leaf][root].is_unique:
+            print(f'all of the records for leaf: {leaf} are the same. NO SPLIT')
+            splits.update({leaf:"no split"})
+            return
         else:
-            print(f'values are not unique perform split')
-            dataframes.update({attribute:df})
-    print(dataframes)
+            print(f'all of the records for leaf: {leaf} are NOT the same. SPLIT')
+            splits.update({leaf:"split"})
     
-    for attribute in dataframes:
-        print(f"processing {attribute}")
-        ID3(root,dataframes[attribute],test_set)
-        
-        
-    print("*****************")
+    for leaf,split in splits.items():
+        if root in training_set:
+            c1 = len(training_set[training_set[root] == leaf].index)
+            F1 = len(training_set[root].index)
+            alpha = c1/F1
+            if split == "split" and alpha < threshold:
+                print(f"setting {leaf} as the new dataset")
+                training_set = training_set[training_set[root] == leaf].drop(columns=root)
+                ID3(root,training_set,test_set,threshold,g)
 
     
 
@@ -214,31 +111,13 @@ def BayesClassifier(training_set,test_set):
 
 # prompt user to select either ID3 or Bayes classifier.
 selection = "ID3" #= input("Please enter your selection for either ID3 or Bayes classification: ")
-threshold = 0.9   #= input("Please enter a threshold: ")
+threshold = 0.5   #= input("Please enter a threshold: ")
 g         = 0.05   #= input("Please enter a value for g: ")
 
 root = ""
 if(selection == "ID3"):
-    print('***********************************')
-    print('TRAINING SET')
-    print(training_set_ID3)
-    print('***********************************')
-    
-    print('***********************************')
-    print('TEST SET')
-    print(test_set_ID3)
-    print('***********************************')
-    ID3(root,training_set,test_set)
+    ID3(root,training_set_ID3,test_set_ID3, threshold, g)
 
 if(selection == "Bayes"):
-    print('***********************************')
-    print('TRAINING SET')
-    print(training_set_Bayes)
-    print('***********************************')
-    
-    print('***********************************')
-    print('TEST SET')
-    print(test_set_Bayes)
-    print('***********************************')
     BayesClassifier(training_set_Bayes,test_set_Bayes)
 
