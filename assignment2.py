ef769e16d260726341ee70f561eb21a7830797b9
Evan Gertis <physevangertis@gmail.com>
2021-11-27 19:39:49 -0500

    add inputs

--- assignment2.py
+++ assignment2.py
@@ -1,18 +1,13 @@
-from numpy.core.defchararray import count
 import pandas as pd
 import numpy as np
 import math
-from sklearn.decomposition import PCA
-from numpy import linalg as LA
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.naive_bayes import GaussianNB
 
 def calculate_metrics(training_set,test_set,classAttribute,classValue):
     # calculate the accuracy, error rate, sensitivity, specificity, and precision for the selected classifier in reference to the corresponding test set.
     tp = len(training_set[training_set[classAttribute] == classValue].index)
     fp = len(test_set[test_set[classAttribute] == classValue].index)
     tn = len(training_set[training_set[classAttribute] == classValue].index) 
-    fn = len(test_set[test_set[classAttribute] != classValue.index])
+    fn = len(test_set[test_set[classAttribute] != classValue].index)
     p  = tp + fp
     n  = tn + fn
     print(f" \t      \t\t {classValue} \t not {classValue} \t \t TOTAL")
@@ -148,9 +143,12 @@ def BayesClassifier(training_set,test_set):
     calculate_metrics(training_set,test_set,classAttribute,classWithMaxValue)
 
 # prompt user to select either ID3 or Bayes classifier.
-selection = "Bayes" #= input("Please enter your selection for either ID3 or Bayes classification: ")
-threshold = 0.5   #= input("Please enter a threshold: ")
-g         = 0.01   #= input("Please enter a value for g: ")
+threshold=None
+g=None
+selection = input("Please enter your selection for either ID3 or Bayes classification: ")
+if selection == "ID3":
+    threshold = input("Please enter a threshold: ")
+    g         = input("Please enter a value for g: ")
 
 root = ""
 if(selection == "ID3"):
